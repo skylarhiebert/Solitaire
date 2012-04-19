d0feba96cbb005fef5ba87349e932ca1fd276a12
Skylar Hiebert <skylar.hiebert@computer.org>
2012-04-19 16:00:52 -0700

    Added card class

---
+++ Solitaire/Card.h
@@ -0,0 +1,13 @@
+//
+//  Card.h
+//  Solitaire
+//
+//  Created by Skylar Hiebert on 4/19/12.
+//  Copyright (c) 2012 skylarhiebert.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Card : NSObject
+
+@end
---
+++ Solitaire/Card.m
@@ -0,0 +1,13 @@
+//
+//  Card.m
+//  Solitaire
+//
+//  Created by Skylar Hiebert on 4/19/12.
+//  Copyright (c) 2012 skylarhiebert.com. All rights reserved.
+//
+
+#import "Card.h"
+
+@implementation Card
+
+@end
